c1c6333da881b1a635f301d32f54997097dcec05
jogo <anaelisaandrade65@gmail.com>
2024-11-01 11:08:37 -0300

    atualizações

--- infinite_runner/MainPage.xaml.cs
+++ infinite_runner/MainPage.xaml.cs
@@ -1,10 +1,54 @@
 ﻿namespace infinite_runner;
 
-public partial class MainPage : ContentPage
+public partial class MainPage : ContentPage;
+
+bool estaMorto = false;
+bool estaPulando = false;
+const int tempoEntreFrames = 25;
+int velocidade1 = 0;
+int velocidade2 = 0;
+int velocidade3 = 0;
+int velocidade2 = 0;
+int velocidade = 0;
+int larguraJanela = 0;
+int alturaJanela = 0;
 {
 	public MainPage()
-	{
-		InitializeComponent();
-	}
+{
+	InitializeComponent();
+}
+protected overide void OnSizeAllocated(double w, double h)
+{
+	base.OnSizeAllocated(w, h);
+	CorrigeTamanhoCenario(w, h);
+	CalculaVelocidade(w);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
